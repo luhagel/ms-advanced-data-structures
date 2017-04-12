60279a9c6b34deb2e000864c564f77944ed8894d
Luca Hagel <hagel.luca@gmail.com>
2017-04-11 22:28:05 -0700

    sorting methods

--- src/sorting.js
+++ src/sorting.js
@@ -1,16 +1,51 @@
 function bubbleSort(arr) {
-  let sortedArr = []
-  return sortedArr
+  var swapped = true;
+  var tmp;
+  for (var j = 0; swapped; j++) {
+    swapped = false;
+    for (var i = 0; i < arr.length - j; i++) {
+      if (arr[i] > arr[i + 1]) {
+        tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
-  let sortedArr = []
-  return sortedArr
+  for (var i = 0; i < arr.length; i++) {
+    var min = arr[i];
+    var swapIndex;
+    for (var j = i; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        swapIndex = j;
+      }
+    }
+    var tmp = arr[i];
+    arr[i] = min;
+    arr[swapIndex] = tmp;
+  }
+  return arr;
 }
 
 function insertionSort(arr) {
-  let sortedArr = []
-  return sortedArr
+  for (var i = 0; i < arr.length; i++) {
+    j = i
+
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      var tmp = arr[j]
+      arr[j] = arr[j - 1]
+      arr[j - 1] = tmp
+
+      j -= 1
+    }
+  }
+
+  return arr
 }
 
 module.exports = {
